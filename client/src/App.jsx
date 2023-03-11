307852ab2188e4c6844e1db19a2264a2751559a1
lekhnath khanal <zsagarkhanal@gmail.com>
2023-03-11 20:59:35 +0530

    client update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+// import Post from './components/Post/Post'
 import Home from './components/Home/Home'
 import ChatbotMain from './components/chatbot/chatbot-main'
+// import {Post} from "./components/Post/Post.jsx"
 
-function App() {
+
+const App = () => {
   return (
     <div>
+          <div>
 
-    <ChatbotMain/>
-    <Home/>
-
+<ChatbotMain/>
+<Home/>
+{/* <Post/> */}
+</div>
     </div>
   )
 }
 
-export default App
+
+export default App;
---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Post = () => {
+
+  return (
+
+    <>
+    <div>
+        <img src='' alt='Post image' />
+        <h5>Title</h5>
+    </div>
+    
+    </>
+  )
+}
+
+export default Post
