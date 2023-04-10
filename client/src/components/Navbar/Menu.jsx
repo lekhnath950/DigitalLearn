9687385fbf6465dc4e2e95594b30a973c4ee897f
lekhnath khanal <zsagarkhanal@gmail.com>
2023-04-10 15:43:47 +0530

    update

--- client/src/components/Navbar/Menu.jsx
+++ client/src/components/Navbar/Menu.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeIcon from '@mui/icons-material/Home';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from '../../firebase';
+
 
 const Menu = () => {
 
     const [dialog, setDialog] = React.useState(false)
+    const [vid, setVid] = useState(undefined)
+    const [vidPer, setVidPer] = useState(0)
+    const [img, setImg] = useState(undefined)
+    const [imgPer, setImgPer] = useState(0)
+    const [tags, setTags] = useState([])
+    const [inputs, setInputs] = useState({})
+
+    const Tag = (e) => {
+        setTags(e.target.value.split(","))
+    }
 
     const DialogBox = () => {
         setDialog(true)
@@ -13,6 +26,49 @@ const Menu = () => {
     const handleClose = () => {
         setDialog(false)
     }
+
+    const handleInputs =(e) => {
+        setInputs(prev=>{
+            return {...prev, [e.target.name]: e.target.value}
+        })
+    }
+
+    const upload = (file, urlType) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, 'posts/' + fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+  (snapshot) => {
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    urlType === "imgUrl" ? setImgPer(Math.round(progress)) : setVidPer(Math.round(progress))
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+        default:
+            break;
+    }
+  }, 
+
+  () => {
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      setInputs((prev)=> {
+        return{...prev, [urlType]: downloadURL}
+      })
+    });
+  }
+);
+
+    }
+
+    useEffect(()=> {vid && upload(vid)}, [vid, "videoUrl" ])
+    useEffect(()=> {img && upload(img)}, [img, "imgUrl"])
+
   return (
     <div className='menu-main'>
         <div className='menus'>
@@ -47,11 +103,22 @@ const Menu = () => {
             <DialogTitle>Upload</DialogTitle>
             <DialogContent>
                 <form>
-                    <input type="text" placeholder="Title" />
-                    <input type="file" accept='video/*' placeholder='Video' />
-                    <input type="text" placeholder='description' />
-                    <input type="text" placeholder="tags" />
-                    <input type="text" placeholder="thumbnail" />
+                    <input type="text" placeholder="Title" name='title' onChange={handleInputs} />
+                    {
+                   vidPer>0?("uploading" + vidPer) :(
+                       <input type="file" accept='video/*' placeholder='Video' onChange={(e)=> setVid(e.target.files[0])} />
+
+                   )
+                    }    
+                    <input type="text" placeholder='description' name="desc" onChange={handleInputs} />
+                    <input type="text" placeholder="tags" onChange={Tag} />
+
+                    {
+                        imgPer>0? ("uploading.." + imgPer) : (
+                            <input type="file" accept='image/*' placeholder="thumbnail" onChange={(e)=> setImg(e.target.files[0])} />
+
+                        ) 
+                        }
                     <Button variant='outlined'>Upload</Button>
                 </form>
             </DialogContent>
---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyApX3kmdtCae8H-mkzv841vxdI5RQaRGGQ",
+  
+    authDomain: "project1-f9f3e.firebaseapp.com",
+  
+    projectId: "project1-f9f3e",
+  
+    storageBucket: "project1-f9f3e.appspot.com",
+  
+    messagingSenderId: "415022680187",
+  
+    appId: "1:415022680187:web:ffea9fdc196e827e6afade"
+  
+  };
+  
+const app = initializeApp(firebaseConfig);
+
+export default app;
